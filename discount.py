60cc9281ee212d9c3063c6ca219f56d8df8372e2
KunchalaRevanthKumar <112418458+KunchalaRevanthKumar@users.noreply.github.com>
2023-01-28 10:52:35 +0530

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,119 @@
+#applying discount in shopping malls
+"""n=int(input())
+c=input()
+if((n>=1000 and n<=2000) and (c=='R' or c=='r' or c=='N' or c=='n')):
+    if c=='r' or c=='R':
+        print(n)
+        b=0
+        print("discount =",b)
+        print("total=",(n-b))
+    else:
+        print(n)
+        b=0.05*n
+        print("discount=",b)
+        print("total=",(n-b))
+elif((n>=2001 and n<=2999) and (c=='R' or c=='r' or c=='N' or c=='n')):
+    if c=='r' or c=='R':
+        print(n)
+        b=0.05*n
+        print("discount =",b)
+        print("total=",(n-b))
+    else:
+        print(n)
+        b=0.1*n
+        print("discount=",b)
+        print("total=",(n-b))
+elif(n>=3000 and (c=='R' or c=='r' or c=='N' or c=='n')):
+    if c=='r' or c=='R':
+        print(n)
+        b=0.15*n
+        print("discount =",b)
+        print("total=",(n-b))
+    else:
+        print(n)
+        b=1.5*n
+        print("discount=",b)
+        print("total=",(n-b))
+else:
+    print("no discount")"""
+
+
+
+"""n=int(input())
+a=11
+for i in range(1,n+1):
+    if i<n:
+        print(a*a,end=",")
+    else:
+        print(a*a)
+    a=a+4"""
+
+
+"""
+a={}
+b=97
+for i in range(1,27):
+    c=chr(b)
+    a.update({i:c})
+    b+=1
+print(a)"""
+
+
+a={}
+b=97
+i=1
+while(i<27):
+    c=chr(b)
+    a.update({i:c})
+    b+=1
+    i=i+1
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+        
+        
+    
+        
